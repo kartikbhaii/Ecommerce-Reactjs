717a7d72f5313daff6f8615812028bb9f60114de
Kartikey_Singh <kkartikeysinghh@gmail.com>
2026-06-27 00:59:35 +0530

    Get cart from the backend and display quantity in Header

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -6,19 +6,27 @@ import "./HomePage.css";
 
 export function HomePage() {
   const [products, setProducts] = useState([])
+  const [cart, setCart] = useState([])
 
   useEffect(() => {
     axios.get("http://localhost:3000/api/products")
     .then((response) => {
       setProducts(response.data);
     });
+
+    // backend is running on localhost:3000, and we need to get data from backend to show on cart-items. so we're using this url:
+    axios.get("http://localhost:3000/api/cart-items")
+    .then((response)=>{
+      setCart(response.data);
+    })
+
   }, []);
 
   return (
     <>
       <link rel="icon" type="image/svg+xml" href="home-favicon.png" />
       <title>Ecommerce Project</title>
-      <Header />
+      <Header cart={cart}/>
       <div className="home-page">
         <div className="products-grid">
           {products.map((product) => {
